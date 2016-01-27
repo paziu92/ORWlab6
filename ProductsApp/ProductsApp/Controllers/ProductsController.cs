05573b21586748cf17f073d3e3f6930d03414f6f
paziu92 <kizap@o2.pl>
2016-01-27 13:58:59 +0100

    lab6

---
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Zupa", Category = "Groceries", Price = 2 },
+            new Product { Id = 2, Name = "danie drugie", Category = "Groceries", Price = 4.25M },
+            new Product { Id = 3, Name = "dolewka", Category = "Groceries", Price = 14.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
